36c232752c749cc4f305fa7b858faf46b810d8d8
André C <35017477+AndreeChamoun@users.noreply.github.com>
2025-05-07 20:59:25 +0200

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 import PropTypes from "prop-types";
-import Marker from "./Marker"; // Adjust the path as needed
+import Marker from "../../components/Marker"; // Adjust the path as needed
 
 const Button = ({
   icon,
